9513ef9d094388309e7e832806253c1da60fedbb
Fransiscus Emmanuel Bunaren <fransiscusbunaren@gmail.com>
2020-12-26 13:55:52 +0000

    Add input to main.sh

--- main.sh
+++ main.sh
@@ -10,3 +10,25 @@ echo "3. Configure Webcam"
 echo "4. Configure Sound"
 echo 
 echo "Choose 1-4:"
+read CHOICE
+
+case CHOICE in
+
+	1)
+		bash stream.sh
+ 	;;
+
+	2)
+		bash watch.sh
+ 	;;
+
+	3)
+		echo "==================="
+		echo "SOUND CONFIGURATION"
+		echo "==================="
+	;;
+
+	*)
+		echo "OTHERS"
+	;;
+esac
