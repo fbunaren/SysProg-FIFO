d4b296564d8103999e361e2ff5af47f8d11eb1c0
Fransiscus Emmanuel Bunaren <fransiscusbunaren@gmail.com>
2020-12-29 10:07:47 +0000

    Implement Sound Configuration for snd_hda_intel Driver

--- sound-config.sh
+++ sound-config.sh
@@ -5,9 +5,30 @@ echo "==================="
 echo "1. Enable Intel HD audio interface."
 echo "2. Disable Intel HD audio interface."
 echo ""
-echo "3. Automatic power-saving timeout"
+echo "3. Automatic power-saving (10 seconds)"
+echo "4. Disable power-saving"
 
 echo ""
-echo "Choose 1-3:"
+echo "Choose 1-4:"
 read SOUND_CHOICE
+
+case $SOUND_CHOICE in
+
+        1)
+                echo 1 > /sys/module/snd_hda_intel/parameters/enable
+        ;;
+
+        2)
+                echo 0 > /sys/module/snd_hda_intel/parameters/enable
+        ;;
+
+        3)
+                echo 10 > /sys/module/snd_hda_intel/parameters/power_save
+        ;;
+
+        4)
+                echo 0 > /sys/module/snd_hda_intel/parameters/power_save
+        ;;
+
+esac
 
