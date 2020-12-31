1c6718a4548713ff542a8d1b1bd77aea3921cf0f
Fransiscus Emmanuel Bunaren <fransiscusbunaren@gmail.com>
2020-12-31 09:37:15 +0000

    Implement Webcam Config

--- webcam-config.sh
+++ webcam-config.sh
@@ -4,11 +4,30 @@ echo "WEBCAM CONFIGURATION"
 echo "Driver: uvcvideo"
 echo "==================="
 
-BIN = 0
+BIN=$(cat /sys/module/uvcvideo/parameters/quirks)
+BIN=$(python3 -c "print(int('$BIN',16))")
 
-read -p "1. Set Fix Bandwidth (y/n)" FIX_BANDWIDTH
-read -p "2. Restrict Frame Rate (y/n)" RESTRICT_FRAME_RATE
+read -r -p "1. Set Fix Bandwidth (y/n)" FIX_BANDWIDTH
+read -r -p "2. Restrict Frame Rate (y/n)" RESTRICT_FRAME_RATE
 
-if ["$FIX_BANDWIDTH" = "y"]; then
-	
+if [[ "$FIX_BANDWIDTH" == "y" ]] 
+then
+	BIN=$(python3 -c "print(int('$BIN',16) + 128)")
+else
+	BIN=$(python3 -c "print(int('$BIN',16) - 128)")
 fi
+
+if [[ "$RESTRICT_FRAME_RATE" = "y" ]] 
+then
+	BIN=$(python3 -c "print(int('$BIN',16) + 512)")
+else
+	BIN=$(python3 -c "print(int('$BIN',16) - 512)")
+fi
+
+# Convert BIN to HEX
+BIN=$(python3 -c "print(hex('$BIN'))")
+
+# Save Configuration
+sudo rmmod uvcvideo
+sudo modprobe uvcvideo quirks=$BIN
+
